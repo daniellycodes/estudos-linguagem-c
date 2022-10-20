7dd932a3d1c95f31fe98aee519cc6d1f4b3bd5ce
Danielly Silva <98064933+daniellycodes@users.noreply.github.com>
2022-10-20 11:23:39 -0300

    Update adivinhacao.c

--- adivinhacao.c
+++ adivinhacao.c
@@ -16,6 +16,24 @@ int main(){
   printf("Qual é o seu chute? ");
   scanf("%i", &chute);
   printf("Seu chute foi %i.\n", chute);
+
+  int acertou = (chute == numeroSecreto);
+
+  if(acertou){
+    printf("Parabéns! Você acertou!\n");
+    printf("Jogue de novo! Você é um bom jogador!\n");
+  }
+  else{
+    
+    int maior = (chute > numeroSecreto);
+    
+    if(maior){
+      printf("Seu chute foi maior que o número secreto\n");
+    }
+    else{
+      printf("Seu chute foi menor que o número secreto\n");
+    }
+  }
    
   return 0;
 }
