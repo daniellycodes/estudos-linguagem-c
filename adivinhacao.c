ad5ece9e3c6ff57b0c4dfb59420c1600a5ef1bfb
Danielly Silva <98064933+daniellycodes@users.noreply.github.com>
2022-10-20 10:13:45 -0300

    Update adivinhacao.c

--- adivinhacao.c
+++ adivinhacao.c
@@ -5,8 +5,17 @@
 int main(){
 
   setlocale(LC_ALL, "portuguese");
-  
-  printf("Bem-vindo ao nosso jogo de adivinhação\n");
-  
+
+  printf("******************************************\n");
+  printf("* Bem-vindo ao nosso jogo de adivinhação *\n");
+  printf("******************************************\n");
+
+  int numeroSecreto = 42;
+  int chute;
+
+  printf("Qual é o seu chute? ");
+  scanf("%i", &chute);
+  printf("Seu chute foi %i.\n", chute);
+   
   return 0;
 }
