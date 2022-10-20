6ea59b961469a8e30d1f680f5ea024754ee70883
Danielly Silva <98064933+daniellycodes@users.noreply.github.com>
2022-10-20 09:51:43 -0300

    Create adivinhacao.c

---
+++ adivinhacao.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+
+  setlocale(LC_ALL, "portuguese");
+  
+  printf("Bem-vindo ao nosso jogo de adivinhação\n");
+  
+  return 0;
+}
